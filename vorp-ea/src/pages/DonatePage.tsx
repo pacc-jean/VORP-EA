afaaac148539142819a1fec1112efb75a1d3b547
jean luc <jeanluc.xii.iv@gmail.com>
2025-07-05 23:03:17 +0300

    donate page base

---
+++ vorp-ea/src/pages/DonatePage.tsx
@@ -0,0 +1,12 @@
+import DonateHero from "../components/donate/DonateHero";
+import WhyGive from "../components/donate/WhyGive";
+
+export default function DonatePage() {
+  return (
+    <>
+      <DonateHero />
+      <WhyGive />
+      {/* Add more sections here */}
+    </>
+  );
+}
