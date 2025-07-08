7b61ec5eb98bc9be6b0daf98808c7a76f5854246
jean luc <jeanluc.xii.iv@gmail.com>
2025-07-08 13:45:39 +0300

    styling tweak

--- vorp-ea/src/components/consistent/MobileBottomNav.tsx
+++ vorp-ea/src/components/consistent/MobileBottomNav.tsx
@@ -206,7 +206,7 @@ export default function MobileBottomNav() {
                                                 ${
                                                     isActive
                                                         ? 'text-green-700 bg-green-100'
-                                                        : 'text-gray-800 hover:bg-gray-100'
+                                                        : 'text-gray-800 bg-gray-100'
                                                 }`}
                                         >
                                             {name}
